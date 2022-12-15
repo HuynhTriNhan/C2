766e7d936d0b8f6bf785866870782527a1aa6b20
HUYNH TRI NHAN <huynhtrinhan6282710@gmail.com>
2022-12-15 15:17:21 +0700

    sua luc 3h15

--- Source.cpp
+++ Source.cpp
@@ -1,21 +1,29 @@
 #include <iostream>
-#include <fstream>
 #include <string>
 #include <vector>
+#include <fstream>
+#include <iomanip>
 #include <chrono>
 #include <ctime>
 
 #define mdl 1000000007
 using namespace std;
 
+int max(int a, int b)
+{
+	if (a > b) return a;
+	return b;
+}
+
 vector<pair<int, int>> bruteforce(string base, string s, int& cp)
 {
 	cp = 0;
 	vector<pair<int, int>>rs;
-	for (int i = 0; i < base.length() - s.length(); i++)
+	for (int i = 0; i < base.length() - s.length() + 1; i++)
+	{
 		for (int j = 0; j < s.length(); j++)
 		{
-			cp++;
+			cp+=2;
 			if (base[i + j] != s[j])
 				break;
 			if (j == s.length() - 1)
@@ -26,6 +34,9 @@ vector<pair<int, int>> bruteforce(string base, string s, int& cp)
 				rs.push_back(a);
 			}
 		}
+		cp+=2;
+	}
+	cp++;
 	return(rs);
 }
 
@@ -37,16 +48,18 @@ vector<pair<int, int>>rabinkarp(string base, string s, int& cp)
 	vector<pair<int, int>>rs;
 	for (int i = s.length() - 1; i >= 0; i--)
 	{
+		cp++;
 		hash_s += (s[i] * exponent) % mdl;
 		hash_base += (base[i] * exponent) % mdl;
 		hash_s %= mdl;
 		hash_base %= mdl;
 		exponent *= 10;
 	}
+	cp++;
 	exponent /= 10;
-	for (int i = 0; i < base.length() - s.length(); i++)
+	for (int i = 0; i < base.length() - s.length() + 1; i++)
 	{
-		cp++;
+		cp += 2;
 		if (hash_base == hash_s)
 		{
 			pair<int, int>a;
@@ -54,8 +67,11 @@ vector<pair<int, int>>rabinkarp(string base, string s, int& cp)
 			a.second = i + s.length() - 1;
 			rs.push_back(a);
 		}
-		hash_base = 10 * (hash_base - (base[i] * exponent) % mdl) + base[i + s.length()] % mdl;
+		cp++;
+		if (i < s.length() < base.length())
+			hash_base = 10 * (hash_base - (base[i] * exponent) % mdl) + base[i + s.length()] % mdl;
 	}
+	cp++;
 	return(rs);
 }
 
@@ -68,7 +84,7 @@ vector<int> partialmatch(string base, string s, int& cp)
 	int val(0), run(2);
 	while (run < s.length())
 	{
-		cp++;
+		cp += 3;
 		if (s[run - 1] == s[val])
 		{
 			val++;
@@ -79,6 +95,7 @@ vector<int> partialmatch(string base, string s, int& cp)
 		else
 			rs[run++] = 0;
 	}
+	cp++;
 	return(rs);
 }
 vector<pair<int, int>>kmp(string base, string s, int& cp)
@@ -89,10 +106,11 @@ vector<pair<int, int>>kmp(string base, string s, int& cp)
 	int i(0), j(0);
 	while (i + j < base.length())
 	{
-		cp++;
+		cp += 2;
 		if (s[j] == base[i + j])
 		{
 			j++;
+			cp++;
 			if (j == s.length())
 			{
 				pair<int, int>a;
@@ -105,8 +123,9 @@ vector<pair<int, int>>kmp(string base, string s, int& cp)
 		}
 		else
 		{
+			cp++;
 			if (pmtable[j] > -1)
-			{	
+			{
 				j = pmtable[j];
 				i = i + j - pmtable[j];
 			}
@@ -117,62 +136,153 @@ vector<pair<int, int>>kmp(string base, string s, int& cp)
 			}
 		}
 	}
+	cp++;
 	return(rs);
 }
 
-int main(int argc, char** argv)
+//ham phu tro Boyer-Moore
+void badCharHeuristic(string str, int badchar[]) //ham khoi tao mang voi phan tu khong thuoc pat thi gia tri la 1
 {
+	// khoi tao vi tri cho tat ca cac phan tu co the xuan hien trong path la -1
+	for (int i = 0; i < 256; i++)
+		badchar[i] = -1;
+
+	//dien vi tri cuoi cung ma ki tu thi i cua pat xuat hien trong pat
+	for (int i = 0; i < str.size(); i++)
+		badchar[str[i]] = i;
+}
+
+
+vector<pair<int, int>> boyerm(string txt, string pat, int& cmp)
+{
+	vector<pair<int, int>> list;
+	int m = pat.size();
+	int n = txt.size();
+
+	int badchar[256];
+
+	//dien mang badChar
+	badCharHeuristic(pat, badchar);
+
+	int index = 0; // index la vi tri dang xet cua xau text
+
+	while (++cmp && index <= (n - m))
+	{
+		int j = m - 1; //la vi tri dang xet cua xau pat, bat dau tu cuoi chuoi
+
+		//bat dau xet match tu cuoi pat ve dau pat
+		while (++cmp && ++cmp && j >= 0 && pat[j] == txt[index + j])
+			j--;
+
+		if (++cmp && j < 0) //truong hop tim duoc string match, j duyet qua het pat nen co gia tri -1
+		{
+			pair<int, int> a;
+			a.first = index;
+			a.second = index + m - 1;
+			list.push_back(a);
+			cout << "pattern occurs at shift = " << index << " - " << index + m - 1 << endl;
+			if (++cmp && index + m < n) //dich vi tri dang xet o txt len vi tri xuat hien thu 2 cua no trong pat neu khong vuot qua txt.size nguoc lai da het txt thoat chuong trinh 
+			{
+				index += m - badchar[txt[index + m]];
+			}
+			else return list;
+			//s += (s + m < n)? m-badchar[txt[s + m]] : 1;
+
+		}
+		else //truong hop xuat hien ki tu khong match thi ta dich con tro dang xet cua txt toi tri xuat hien miss match
+			index += max(1, j - badchar[txt[index + j]]);
+	}
+	return list;
+}
+
+void commandHandler(string mode, string inputPath, string outputPath, string outputInfo)
+{
+	vector<pair<int, int>> list_index;
+	int count_cmp = 0;
+	int time;
+	//2 bien tren la output, truyen vao ham xu ly roi tra ve
 	ifstream fin;
-	fin.open(argv[2]);
-	string base, s;
-	getline(fin, base);
-	getline(fin, s);
+	fin.open(inputPath);
+	if (!fin.is_open())
+	{
+		cout << "Unable to open file!!!\n";
+		return;
+	}
+
+	string txt, pat;
+	getline(fin, txt);
+	getline(fin, pat);
 	fin.close();
+
+	clock_t start, end;
+	start = clock();
+	cout << start << endl;
+
+	//mode case
+	if (mode == "-bf")
+	{
+		list_index = bruteforce(txt, pat, count_cmp);
+	}
+	else if (mode == "-rk")
+	{
+		list_index = rabinkarp(txt, pat, count_cmp);
+	}
+	else if (mode == "-kmp")
+	{
+		list_index = kmp(txt, pat, count_cmp);
+	}
+
+	else if (mode == "-bm")
+	{
+		list_index = boyerm(txt, pat, count_cmp);
+	}
+	/*
+	else if
+	{
+
+	}
+	*/
+	else
+		return;
+	//calculate time
+	end = clock();
+	cout << end << endl;
+	time = end - start;
+	cout << time << endl;
+	//write file out
 	ofstream fout;
-	fout.open(argv[3]);
-	int cp(0), time;
-	vector<pair<int, int>>rs;
-	if (argv[1] == "-bf")
-	{
-		chrono::time_point<chrono::system_clock> start, end;
-		time_t end_time;
-		chrono::duration<double>elapsed_seconds;
-		start = chrono::system_clock::now();
-		rs = bruteforce(base, s, cp);
-		end = chrono::system_clock::now();
-		elapsed_seconds = end - start;
-		end_time = chrono::system_clock::to_time_t(end);
-		time = elapsed_seconds.count() * 1000;
-	}
-	else if (argv[1] == "-rk")
-	{
-		chrono::time_point<chrono::system_clock> start, end;
-		time_t end_time;
-		chrono::duration<double>elapsed_seconds;
-		start = chrono::system_clock::now();
-		rs = rabinkarp(base, s, cp);
-		end = chrono::system_clock::now();
-		elapsed_seconds = end - start;
-		end_time = chrono::system_clock::to_time_t(end);
-		time = elapsed_seconds.count() * 1000;
-	}
-	else if (argv[1] == "-kmp")
-	{
-		chrono::time_point<chrono::system_clock> start, end;
-		time_t end_time;
-		chrono::duration<double>elapsed_seconds;
-		start = chrono::system_clock::now();
-		rs = kmp(base, s, cp);
-		end = chrono::system_clock::now();
-		elapsed_seconds = end - start;
-		end_time = chrono::system_clock::to_time_t(end);
-		time = elapsed_seconds.count() * 1000;
-	}
-	if (argv[4] == "-t" || argv[4] == "-ind")
-		fout << "Thoi gian thuc hien: " << time << '\n';
-	if (argv[4] == "-n" || argv[4] == "-ind")
-		fout << "So luong phep so sanh: " << cp << '\n';
-	for (int i = 0; i < rs.size(); i++)
-		fout << rs[i].first << ' ' << rs[i].second << '\n';
+	fout.open(outputPath);
+	for (int i = 0; i < list_index.size(); i++)
+		fout << list_index[i].first << ' ' << list_index[i].second << '\n';
 	fout.close();
+
+	//output case
+
+	if (outputInfo == "-t" || outputInfo == "-ind")
+	{
+		cout << "Running time: " << time << "ms\n";
+	}
+
+	if (outputInfo == "-n" || outputInfo == "-ind")
+	{
+		cout << "Comparisons: " << count_cmp;
+	}
+
+
+}
+
+int main(int argc, char* argv[]) {
+	// Xu ly truong hop khong truyen vao du hoac thua tham so
+	if (argc != 5)
+		return 0;
+	// Lay du lieu tu command line
+	string mode = string(argv[1]);
+	string inputPath = string(argv[2]);
+	string outputPath = string(argv[3]);
+	string outputInfo = string(argv[4]);
+
+	//Goi ham xu ly lenh
+	commandHandler(mode, inputPath, outputPath, outputInfo);
+
+	return 0;
 }
